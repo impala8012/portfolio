380bddebaa78039c7e82234cb078057c7c7cd823
Dylan Lo <impala8012@gmail.com>
2021-08-26 00:58:46 +0800

    fix the bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import GlobalStyle from "./globalStyles";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { HashRouter as Router, Switch, Route} from "react-router-dom"
 import { Navbar, Footer } from "./components";
 import HomePage from "./pages/HomePage/HomePage";
 import ExperiencePage from "./pages/ExperiencePage/ExperiencePage";
@@ -13,9 +13,9 @@ function App() {
       <ScrollTop />
       <Navbar />
       <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/projects" component={ProjectPage} />
-        <Route path="/experience" component={ExperiencePage} />
+        <Route exact path="/" exact component={HomePage} />
+        <Route exact path="/projects" component={ProjectPage} />
+        <Route exact path="/experience" component={ExperiencePage} />
       </Switch>
       <Footer />
     </Router>
